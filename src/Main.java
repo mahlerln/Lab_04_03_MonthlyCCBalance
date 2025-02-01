7ee55a64ea07cc0be5b645b55610b20580fa8eaa
Logan <mahlerln@mail.uc.edu>
2025-01-31 20:45:50 -0500

    Final Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        double cardBalance = 5000;
+        double interestRate = 0.17;
+        double monthOneInterest = cardBalance * interestRate;
+        double monthOneTotal = monthOneInterest + cardBalance;
+        double monthTwoInterest = monthOneTotal * interestRate;
+        double monthTwoTotal = monthTwoInterest + monthOneTotal;
+
+
+
+
+        System.out.printf("Starting card balance is $" + cardBalance);
+        System.out.printf(" interest after one month is $" + monthOneInterest);
+        System.out.printf(" total after one month is $" + monthOneTotal);
+        System.out.printf(" interest for month two is $" + monthTwoInterest);
+        System.out.printf(" total after two months is $" + monthTwoTotal);
+
+
     }
 }
